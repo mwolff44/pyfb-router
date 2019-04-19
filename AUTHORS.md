2a2cb8f4edfbd5b6431cf0fb031fa595232e7954
Mathias WOLFF <mathias@celea.org>
2019-04-19 11:22:31 +0200

    mix for publishing image

---
+++ AUTHORS.md
@@ -0,0 +1,9 @@
+# Credits
+
+## Development Lead
+
+- Mathias WOLFF [mwolff44](https://github.com/mwolff44)
+
+## Contributors
+
+None yet. Why not be the first?%  
---
+++ version/version.go
@@ -0,0 +1,21 @@
+package version
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// GitCommit is The git commit that was compiled. This will be filled in by the compiler.
+var GitCommit string
+
+// Version is The main version number that is being run at the moment.
+const Version = "0.1.0"
+
+// BuildDate is the build date
+var BuildDate = ""
+
+// GoVersion is the go version
+var GoVersion = runtime.Version()
+
+//OsArch is the OS architecture
+var OsArch = fmt.Sprintf("%s %s", runtime.GOOS, runtime.GOARCH)
