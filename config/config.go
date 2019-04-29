a901aae4333f0f7f38603fcfe13e4bd5f430ab0f
Mathias WOLFF <mathias@celea.org>
2019-04-29 10:56:52 +0200

    only use env for config

--- config/config.go
+++ config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"log"
 	"path/filepath"
 
 	"github.com/spf13/viper"
@@ -12,24 +11,10 @@ var config *viper.Viper
 // Init is an exported method that takes the environment starts the viper
 // (external lib) and returns the configuration struct.
 func Init(env string) {
-	var err error
 	config = viper.New()
-	config.SetDefault("server.port", "127.0.0.1:8081")
-	config.SetDefault("GIN_MODE", "debug")
-	config.SetDefault("POSTGRES_HOST", "postgres")
-	config.SetDefault("POSTGRES_PORT", "5432")
-	config.SetDefault("POSTGRES_DB", "pyfreebilling")
-	config.SetDefault("POSTGRES_USER", "pyfreebilling")
-	config.SetDefault("POSTGRES_PASSWORD", "secret")
-	config.SetConfigType("yaml")
-	config.SetConfigName(env)
-	config.AddConfigPath("../config/")
-	config.AddConfigPath("config/")
-	config.AddConfigPath(".")
-	err = config.ReadInConfig()
-	if err != nil {
-		log.Fatal("error on parsing configuration file")
-	}
+	config.AutomaticEnv()
+	config.SetEnvPrefix("")
+
 }
 
 func relativePath(basedir string, path *string) {
